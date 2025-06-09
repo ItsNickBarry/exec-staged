d7f73a1cf9bf126017f9ad33a481f4606bfb0bfc
Nick Barry <itsnickbarry@protonmail.ch>
2025-06-08 22:16:54 -0600

    handle errors at each stage of lifecycle

--- src/bin/exec_staged.ts
+++ src/bin/exec_staged.ts
@@ -7,69 +7,106 @@ const STASH_MESSAGE = `💾 ${pkg.name} backup stash`;
 
 const git = simpleGit();
 
-const version = await git.version();
+const dropBackupStash = async () => {
+  try {
+    console.log('➡️ Dropping backup stash...');
+    await git.stash(['drop', 'stash@{0}']);
+  } catch (error) {
+    console.log('⚠️ Failed to drop backup stash!');
+    process.exit(1);
+  }
+};
 
-if (version.major < 2 || (version.major === 2 && version.minor < 14)) {
-  console.log('⚠️ Unsupported git version!');
+const restoreBackupStash = async () => {
+  try {
+    console.log('➡️ Restoring state from backup stash...');
+    await git.reset(['--hard', 'HEAD']);
+    await git.stash(['apply', '--index', 'stash@{0}']);
+    await dropBackupStash();
+  } catch (error) {
+    console.log('⚠️ Failed to restore state from backup stash!');
+    process.exit(1);
+  }
+};
+
+try {
+  const version = await git.version();
+
+  if (version.major < 2 || (version.major === 2 && version.minor < 14)) {
+    console.log('⚠️ Unsupported git version!');
+    process.exit(1);
+  }
+} catch (error) {
+  console.log('⚠️ Git installation not found!');
   process.exit(1);
 }
 
 const list = await git.stash(['list']);
 
 if (list.includes(STASH_MESSAGE)) {
-  console.log(`⚠️ Found unexpected ${pkg.name} backup stash!`);
+  console.log('⚠️ Found unexpected backup stash!');
   console.log(
     'It must be left over from a previous failed run.  Remove it before proceeding.',
   );
   process.exit(1);
 }
 
-console.log('➡️ Creating backup stash and hiding unstaged changes...');
-await git.stash([
-  'push',
-  '--keep-index',
-  '--include-untracked',
-  '--message',
-  STASH_MESSAGE,
-]);
+try {
+  console.log('➡️ Creating backup stash and hiding unstaged changes...');
+  // TODO: keep unstaged deletions in index
+  await git.stash([
+    'push',
+    '--keep-index',
+    '--include-untracked',
+    '--message',
+    STASH_MESSAGE,
+  ]);
+} catch (error) {
+  console.log('⚠️ Error creating backup stash!');
+  process.exit(1);
+}
 
 // TODO: restore merge status
 
 const tasks = ['prettier --write .', 'knip'];
 
-console.log('➡️ Running tasks...');
+try {
+  console.log('➡️ Running tasks...');
 
-for (const task of tasks) {
-  const result = child_process.spawnSync('pnpm', task.split(' '), {
-    stdio: 'inherit',
-  });
+  for (const task of tasks) {
+    const result = child_process.spawnSync('pnpm', task.split(' '), {
+      stdio: 'inherit',
+    });
 
-  if (result.status !== 0) {
-    console.log(`⚠️ Error running task: \`${task}\`!`);
-    console.log('➡️ Restoring state from backup stash...');
-    await git.reset(['--hard', 'HEAD']);
-    await git.stash(['apply', '--index', 'stash@{0}']);
-    console.log('➡️ Dropping backup stash...');
-    await git.stash(['drop', 'stash@{0}']);
-    process.exit(1);
+    if (result.status !== 0) {
+      console.log(`⚠️ Error running task: \`${task}\`!`);
+      await restoreBackupStash();
+      process.exit(1);
+    }
   }
+} catch (error) {
+  console.log('⚠️ Uncaught error running tasks!');
+  await restoreBackupStash();
+  process.exit(1);
 }
 
-console.log('➡️ Adding changes made by tasks...');
-await git.add(['-A']);
+try {
+  console.log('➡️ Adding changes made by tasks...');
+  await git.add(['-A']);
+} catch (error) {
+  console.log('⚠️ Error adding new changes!');
+  await restoreBackupStash();
+  process.exit(1);
+}
 
 try {
   console.log('➡️ Restoring unstaged changes...');
   await git.stash(['apply', '--index', 'stash@{0}']);
 } catch (error) {
   console.log('⚠️ Error restoring unstaged changes!');
-  console.log('➡️ Restoring state from backup stash...');
-  await git.reset(['--hard', 'HEAD']);
-  await git.stash(['apply', '--index', 'stash@{0}']);
-  console.log('➡️ Dropping backup stash...');
-  await git.stash(['drop', 'stash@{0}']);
+  await restoreBackupStash();
   process.exit(1);
 }
 
-console.log('➡️ Dropping backup stash...');
-await git.stash(['drop', 'stash@{0}']);
+await dropBackupStash();
+process.exit(0);
