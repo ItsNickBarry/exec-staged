93de07bbd4ff0ae110f4104c5b9bb808afcff300
Nick Barry <itsnickbarry@protonmail.ch>
2025-06-10 12:21:39 -0600

    return all fixtures from setup function

--- test/fixtures.ts
+++ test/fixtures.ts
@@ -4,13 +4,14 @@ import fs from 'node:fs';
 import path from 'node:path';
 import { simpleGit } from 'simple-git';
 
-export const cwd = path.resolve(envPaths(pkg.name).temp, 'test');
-
-export const git = simpleGit(cwd);
-
-export const initializeTestRepository = async () => {
+export const setup = async () => {
+  const cwd = path.resolve(envPaths(pkg.name).temp, 'test');
   await fs.promises.rm(cwd, { recursive: true, force: true });
   await fs.promises.mkdir(cwd, { recursive: true });
+
+  const git = simpleGit(cwd);
   await git.init();
   await git.commit('initial commit', ['--allow-empty']);
+
+  return { cwd, git };
 };
