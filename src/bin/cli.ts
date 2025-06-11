6c93ae867536418538b3efe8a29d371a02a4bd7e
Nick Barry <itsnickbarry@protonmail.ch>
2025-06-10 23:19:34 -0600

    remove hardcoded tasks, support passing multiple tasks as cli arguments

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -3,14 +3,11 @@ import execStaged from '../index.js';
 import { loadConfig, parseTasks } from '../lib/config.js';
 
 const cwd = process.cwd();
-
 const config = await loadConfig(cwd);
 
 const configTasks = await parseTasks(Object.values(config));
 
-// const tasks = Object.values(config).flat();
-const tasks = [process.argv[2] ?? ['prettier --write .', 'knip']].flat();
+const tasks = process.argv.length > 2 ? process.argv.slice(2) : configTasks;
 
 const status = await execStaged(cwd, tasks);
-
 process.exit(status);
