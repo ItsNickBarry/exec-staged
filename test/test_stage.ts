f6fe75bcbe36dca9aeb6dbfddba9fd0c75ec00a6
Nick Barry <itsnickbarry@protonmail.ch>
2025-06-11 00:44:21 -0600

    assert non-absolute path in writeFile test helper

--- test/test_stage.ts
+++ test/test_stage.ts
@@ -1,6 +1,7 @@
 import pkg from '../package.json';
 import { Stage } from '../src/lib/stage';
 import envPaths from 'env-paths';
+import assert from 'node:assert';
 import fs from 'node:fs';
 import path from 'node:path';
 import simpleGit, { SimpleGit } from 'simple-git';
@@ -9,6 +10,7 @@ export class TestStage extends Stage {
   declare public readonly git: SimpleGit;
 
   public async writeFile(relativePath: string, contents: string = '') {
+    assert(!path.isAbsolute(relativePath));
     const absolutePath = path.resolve(this.cwd, relativePath);
     await fs.promises.mkdir(path.dirname(absolutePath), { recursive: true });
     await fs.promises.writeFile(absolutePath, contents);
