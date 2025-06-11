8ba447875b0d9ca3a61891a5cbb99be7a5b2d5ce
Nick Barry <itsnickbarry@protonmail.ch>
2025-06-11 00:11:08 -0600

    add ExitCode type

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Stage } from './lib/stage.js';
+import type { ExitCode } from './types.js';
 
-export default async (cwd: string, tasks: string[]): Promise<number> => {
+export default async (cwd: string, tasks: string[]): Promise<ExitCode> => {
   try {
     const stage = new Stage(cwd);
     await stage.exec(tasks);
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+export type ExitCode = 0 | 1;
+
 export type Tasks =
   | string
   | Tasks[]
