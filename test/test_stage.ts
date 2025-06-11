2f18e67e9db6650835af6068ec03b23d7a91ae7b
Nick Barry <itsnickbarry@protonmail.ch>
2025-06-11 01:29:39 -0600

    limit number of exit event handlers

--- test/test_stage.ts
+++ test/test_stage.ts
@@ -6,6 +6,32 @@ import fs from 'node:fs';
 import path from 'node:path';
 import simpleGit, { SimpleGit } from 'simple-git';
 
+const testDirectories: string[] = [];
+
+[
+  'beforeExit',
+  'uncaughtException',
+  'unhandledRejection',
+  'SIGHUP',
+  'SIGINT',
+  'SIGQUIT',
+  'SIGILL',
+  'SIGTRAP',
+  'SIGABRT',
+  'SIGBUS',
+  'SIGFPE',
+  'SIGUSR1',
+  'SIGSEGV',
+  'SIGUSR2',
+  'SIGTERM',
+].forEach((event) =>
+  process.on(event, () =>
+    testDirectories.forEach((dir) =>
+      fs.rmSync(dir, { recursive: true, force: true }),
+    ),
+  ),
+);
+
 export class TestStage extends Stage {
   declare public readonly git: SimpleGit;
 
@@ -24,28 +50,7 @@ export class TestStage extends Stage {
 
   public static async create() {
     const cwd = path.resolve(envPaths(pkg.name).temp, crypto.randomUUID());
-
-    process.setMaxListeners(process.getMaxListeners() + 1);
-
-    [
-      'beforeExit',
-      'uncaughtException',
-      'unhandledRejection',
-      'SIGHUP',
-      'SIGINT',
-      'SIGQUIT',
-      'SIGILL',
-      'SIGTRAP',
-      'SIGABRT',
-      'SIGBUS',
-      'SIGFPE',
-      'SIGUSR1',
-      'SIGSEGV',
-      'SIGUSR2',
-      'SIGTERM',
-    ].forEach((event) =>
-      process.on(event, () => fs.rmSync(cwd, { recursive: true, force: true })),
-    );
+    testDirectories.push(cwd);
     await fs.promises.mkdir(cwd, { recursive: true });
 
     const git = simpleGit(cwd);
