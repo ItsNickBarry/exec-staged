30771094cc2726625f14e0d589bd884d78b0592c
Nick Barry <itsnickbarry@protonmail.ch>
2025-06-13 18:36:38 -0600

    add logger module

---
+++ src/lib/logger.ts
@@ -0,0 +1,30 @@
+import pkg from '../../package.json' with { type: 'json' };
+import envPaths from 'env-paths';
+import fs from 'node:fs';
+import path from 'node:path';
+
+export class Logger {
+  private quiet: boolean;
+  private outFile: string;
+
+  constructor(quiet: boolean = false) {
+    this.quiet = quiet;
+    this.outFile = path.resolve(
+      envPaths(pkg.name).temp,
+      `debug-${new Date().getTime().toString()}.txt`,
+    );
+    fs.mkdirSync(path.dirname(this.outFile), { recursive: true });
+  }
+
+  public log(...params: Parameters<typeof console.log>): void {
+    this.debug(...params);
+
+    if (!this.quiet) {
+      console.log(...params);
+    }
+  }
+
+  public debug(...params: Parameters<typeof console.debug>): void {
+    fs.appendFileSync(this.outFile, params.join('\n') + '\n');
+  }
+}
--- src/lib/stage.ts
+++ src/lib/stage.ts
@@ -1,8 +1,7 @@
-import pkg from '../../package.json' with { type: 'json' };
 import type { StageOptions } from '../types.js';
 import { BACKUP_STASH_MESSAGE, stageLifecycleMessages } from './constants.js';
+import { Logger } from './logger.js';
 import { spawn, spawnSync } from './spawn.js';
-import envPaths from 'env-paths';
 import fs from 'node:fs';
 import path from 'node:path';
 import semver from 'semver';
@@ -10,17 +9,11 @@ import semver from 'semver';
 export class Stage {
   public readonly cwd: string;
   protected stashed: boolean = false;
-  protected quiet: boolean;
-  private debugFile: string;
+  private logger: Logger;
 
   constructor(cwd: string, options: StageOptions = {}) {
     this.cwd = cwd;
-    this.quiet = Boolean(options.quiet);
-    this.debugFile = path.resolve(
-      envPaths(pkg.name).temp,
-      `debug-${new Date().getTime().toString()}.txt`,
-    );
-    fs.mkdirSync(path.dirname(this.debugFile), { recursive: true });
+    this.logger = new Logger(options.quiet);
   }
 
   public async exec(tasks: string[]) {
@@ -31,7 +24,7 @@ export class Stage {
       this.merge();
       this.clean();
     } catch (error) {
-      this.debug(error);
+      this.logger.debug(error);
       this.revert();
       this.clean();
       throw error;
@@ -39,7 +32,7 @@ export class Stage {
   }
 
   protected check() {
-    this.log(stageLifecycleMessages.check);
+    this.logger.log(stageLifecycleMessages.check);
     let version: string | undefined;
 
     try {
@@ -47,27 +40,27 @@ export class Stage {
         /git version (\d+\.\d+\.\d+)/,
       )?.[1];
     } catch (error) {
-      this.log('⚠️ Git installation not found!');
+      this.logger.log('⚠️ Git installation not found!');
       throw error;
     }
 
     if (!version || semver.lte(version, '2.13.0')) {
-      this.log('⚠️ Unsupported git version!');
+      this.logger.log('⚠️ Unsupported git version!');
       throw new Error('TODO: error');
     }
 
     try {
       this.git(['rev-parse', '--is-inside-work-tree']);
     } catch (error) {
-      this.log('⚠️ Not a git repository!');
+      this.logger.log('⚠️ Not a git repository!');
       throw new Error('TODO: error');
     }
 
     const list = this.git(['stash', 'list']);
 
     if (list.includes(BACKUP_STASH_MESSAGE)) {
-      this.log('⚠️ Found unexpected backup stash!');
-      this.log(
+      this.logger.log('⚠️ Found unexpected backup stash!');
+      this.logger.log(
         'It must be left over from a previous failed run.  Remove it before proceeding.',
       );
       throw new Error('TODO: error');
@@ -75,14 +68,16 @@ export class Stage {
   }
 
   protected prepare() {
-    this.log(stageLifecycleMessages.prepare);
+    this.logger.log(stageLifecycleMessages.prepare);
     const status = this.git(['status', '-z']);
 
     // if there are no files in index or working tree, do not attempt to stash
     if (status.length === 0) return;
 
     try {
-      this.debug('➡️ ➡️ Creating backup stash and hiding unstaged changes...');
+      this.logger.debug(
+        '➡️ ➡️ Creating backup stash and hiding unstaged changes...',
+      );
 
       this.git([
         'stash',
@@ -95,7 +90,7 @@ export class Stage {
 
       this.stashed = true;
     } catch (error) {
-      this.log('⚠️ Error creating backup stash!');
+      this.logger.log('⚠️ Error creating backup stash!');
       throw error;
     }
 
@@ -103,34 +98,36 @@ export class Stage {
   }
 
   protected async run(tasks: string[]) {
-    this.log(stageLifecycleMessages.run);
+    this.logger.log(stageLifecycleMessages.run);
 
     for (let i = 0; i < tasks.length; i++) {
       const task = tasks[i];
 
       try {
-        this.log(`➡️ Running task ${i + 1} of ${tasks.length}: \`${task}\`...`);
+        this.logger.log(
+          `➡️ Running task ${i + 1} of ${tasks.length}: \`${task}\`...`,
+        );
         const output = await spawn(this.cwd, task);
-        this.debug(
+        this.logger.debug(
           output
             .split('\n')
             .map((line) => `> ${line}`)
             .join('\n'),
         );
       } catch (error) {
-        this.log(`⚠️ Error running task: \`${task}\`!`);
+        this.logger.log(`⚠️ Error running task: \`${task}\`!`);
         throw error;
       }
     }
   }
 
   protected merge() {
-    this.log(stageLifecycleMessages.merge);
+    this.logger.log(stageLifecycleMessages.merge);
 
     let stash: string | undefined;
 
     if (this.stashed) {
-      this.debug('➡️ ➡️ Cleaning up redundant files in index...');
+      this.logger.debug('➡️ ➡️ Cleaning up redundant files in index...');
 
       // attempt to retrieve the stash before running any damaging operations
       stash = this.findBackupStash();
@@ -159,31 +156,31 @@ export class Stage {
     }
 
     try {
-      this.debug('➡️ ➡️ Adding changes made by tasks...');
+      this.logger.debug('➡️ ➡️ Adding changes made by tasks...');
       this.git(['add', '-A']);
     } catch (error) {
-      this.log('⚠️ Error adding new changes!');
+      this.logger.log('⚠️ Error adding new changes!');
       throw error;
     }
 
     if (!this.stashed) return;
 
     try {
-      this.debug('➡️ ➡️ Restoring unstaged changes from stash...');
+      this.logger.debug('➡️ ➡️ Restoring unstaged changes from stash...');
       this.git(['stash', 'apply', '--index', stash!]);
     } catch (error) {
-      this.log('⚠️ Error restoring unstaged changes from stash!');
+      this.logger.log('⚠️ Error restoring unstaged changes from stash!');
       throw error;
     }
   }
 
   protected revert() {
-    this.log(stageLifecycleMessages.revert);
+    this.logger.log(stageLifecycleMessages.revert);
 
     if (!this.stashed) return;
 
     try {
-      this.debug('➡️ ➡️ Restoring state from backup stash...');
+      this.logger.debug('➡️ ➡️ Restoring state from backup stash...');
 
       // attempt to retrieve the stash before running any damaging operations
       const stash = this.findBackupStash();
@@ -192,28 +189,28 @@ export class Stage {
       this.git(['reset', '--hard', 'HEAD']);
       this.git(['stash', 'apply', '--index', stash]);
     } catch (error) {
-      this.log('⚠️ Failed to restore state from backup stash!');
+      this.logger.log('⚠️ Failed to restore state from backup stash!');
       throw error;
     }
   }
 
   protected clean() {
-    this.log(stageLifecycleMessages.clean);
+    this.logger.log(stageLifecycleMessages.clean);
 
     if (!this.stashed) return;
 
     try {
       this.git(['stash', 'drop', this.findBackupStash()]);
     } catch (error) {
-      this.log('⚠️ Failed to drop backup stash!');
+      this.logger.log('⚠️ Failed to drop backup stash!');
       throw error;
     }
   }
 
   protected git(args: string[]): string {
-    this.debug(`git: ${args.map((arg) => `[${arg}]`).join(' ')}`);
+    this.logger.debug(`git: ${args.map((arg) => `[${arg}]`).join(' ')}`);
     const output = spawnSync(this.cwd, ['git', ...args]);
-    this.debug(
+    this.logger.debug(
       output
         .split('\n')
         .map((line) => `> ${line}`)
@@ -236,17 +233,5 @@ export class Stage {
     } else {
       return '';
     }
-  }
-
-  private log(...params: Parameters<typeof console.log>): void {
-    this.debug(...params);
-
-    if (!this.quiet) {
-      console.log(...params);
-    }
-  }
-
-  private debug(...params: Parameters<typeof console.debug>): void {
-    fs.appendFileSync(this.debugFile, params.join('\n') + '\n');
   }
 }
