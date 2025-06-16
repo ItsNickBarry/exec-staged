a1d6a63af0648bf82c9310c54de5b59cc5f04792
Nick Barry <itsnickbarry@protonmail.ch>
2025-06-16 14:04:17 -0600

    reorder spawn functions

--- src/lib/spawn.ts
+++ src/lib/spawn.ts
@@ -3,24 +3,6 @@ import { registerExitHandler, deregisterExitHandler } from 'on-process-exit';
 import parseArgsStringToArgv from 'string-argv';
 
 /**
- * Spawn a child process synchronously using the `execa` package.
- * This is used to run `git` commands.  A synchronous API is required because
- * cleanup operations may be run via `on-process-exit`.
- *
- * @param cwd Directory where command should be executed.
- * @param args Command string or array of command tokens.
- * @throws ExecaSyncError
- * @returns Execa `SyncResult`.
- */
-export const spawnSync = (cwd: string, args: string | string[]) => {
-  if (typeof args === 'string') {
-    args = parseArgsStringToArgv(args);
-  }
-
-  return execaSync({ cwd })(args[0], args.slice(1));
-};
-
-/**
  * Spawn a child process asynchronously using the `execa` package.
  * This is used to run `exec-staged` tasks.  The `execa` package is
  * required because it provides the `preferLocal` option.
@@ -47,4 +29,22 @@ export const spawn = async (cwd: string, args: string | string[]) => {
   subprocess.once('close', () => deregisterExitHandler(id));
 
   return subprocess;
+};
+
+/**
+ * Spawn a child process synchronously using the `execa` package.
+ * This is used to run `git` commands.  A synchronous API is required because
+ * cleanup operations may be run via `on-process-exit`.
+ *
+ * @param cwd Directory where command should be executed.
+ * @param args Command string or array of command tokens.
+ * @throws ExecaSyncError
+ * @returns Execa `SyncResult`.
+ */
+export const spawnSync = (cwd: string, args: string | string[]) => {
+  if (typeof args === 'string') {
+    args = parseArgsStringToArgv(args);
+  }
+
+  return execaSync({ cwd })(args[0], args.slice(1));
 };
